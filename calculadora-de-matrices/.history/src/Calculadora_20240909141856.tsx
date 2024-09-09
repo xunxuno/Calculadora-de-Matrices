069c12c757761cd6c77b3c6c43aa126466a6498f
xunxuno <sintraxlch@gmail.com>
2024-09-09 16:04:39 -0600

    rep vite

--- calculadora-de-matrices/.history/src/Calculadora_20240909141856.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-import Button from './Button';
-import './Calculadora.css';
-
-function Calculadora() {
-
-}
-
-export default Calculadora;
--- calculadora-de-matrices/src/main.tsx
+++ calculadora-de-matrices/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDoom from 'react-dom/client'
-import Calculadora from './Calculadora.tsx'
+//import Calculadora from './Calculadora.tsx'
 
 
 ReactDoom.createRoot(document.getElementById('root')!).render(
